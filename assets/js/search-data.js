bd4d621c344a65a05fb482f1c22bebde9e88de71
dayusun <111668241+dayusun@users.noreply.github.com>
2025-08-18 00:48:55 +0000

    Deploying to gh-pages from @ dayusun/dayusun.github.io@9507ca1bcdbbd5140970aca2083c084f59614ba8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "My CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
